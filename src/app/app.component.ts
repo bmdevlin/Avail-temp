8facf4b47b7c6535aab044b773023891b18b680a
Barry Devlin <barrymdev@yahoo.com>
2018-04-30 21:55:53 -0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { OneSignal } from '@ionic-native/onesignal';
@@ -11,18 +11,42 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, private onesignal: OneSignal, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, private alertCtrl: AlertController, private onesignal: OneSignal, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-      this.setupPush();
+      if (platform.is('cordova')){
+         this.setupPush();
+        }
     });
   }
 
   setupPush(){
-    this.onesignal.startInit( '7d3751ec-95c7-4e71-a928-b3a7213775b7');
+    this.onesignal.startInit( '7d3751ec-95c7-4e71-a928-b3a7213775b7',
+                                                        '844760076384');
+    
+    this.onesignal.handleNotificationOpened().subscribe(data => {
+      console.log('We opened a push', data);
+      let message = data.notification.payload.body;
+      let title = data.notification.payload.title;
+      let alert = this.alertCtrl.create({
+        title: title,
+        subTitle: message, 
+        buttons: [
+          {
+            text: 'Close',
+            role: 'cancel'
+          }
+        ]
+      })
+      alert.present();
+    });
+ 
+    this.onesignal.handleNotificationReceived().subscribe(data => {
+      console.log('We received a push', data)
+    });
 
     this.onesignal.endInit();
   }
